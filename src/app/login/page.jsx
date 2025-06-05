685f90e9b900d5086bb541da022aeb5ee3eb5189
Atharva <teteatharva@gmail.com>
2025-06-05 20:57:24 +0530

    After taiwind setup

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -146,7 +146,7 @@ const AuthForm = () => {
                 onChange={(e) => setPassword(e.target.value)}
                 disabled={loadingProvider === 'credentials'}
                 className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm disabled:bg-gray-50"
-              /> 
+              />
             </div>
 
             <button
